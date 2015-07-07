5dc4c38e46f585e97b22a0f8b541cfa40e9ffe79
Rich Halliday <r.halliday@factore.ca>
2015-07-06 23:17:06 -0400

    Initial Commit

---
+++ SpriteKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpriteKitDemo
+//
+//  Created by Rich Halliday on 2015-07-06.
+//  Copyright (c) 2015 Factor[e]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpriteKitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpriteKitDemo
+//
+//  Created by Rich Halliday on 2015-07-06.
+//  Copyright (c) 2015 Factor[e]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
